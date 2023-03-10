ee32a9f69e3b80bd2805ba40ee5a51f79f58dd41
jujuling <53365447+jujuling@users.noreply.github.com>
2023-03-10 17:31:23 +0900

    Baekjoon : 9610
    사분면

---
+++ 9610.py
@@ -0,0 +1,13 @@
+n=int(input())
+q1,q2,q3,q4,axis=0,0,0,0,0
+for _ in range(n):
+    x,y=map(int,input().split())
+    if x==0 or y==0 :
+        axis+=1
+    elif x>0 :
+        if y>0: q1+=1
+        else : q4+=1
+    else: #x<0
+        if y>0: q2+=1
+        else:q3+=1
+print("Q1: %d\nQ2: %d\nQ3: %d\nQ4: %d\nAXIS: %d"%(q1,q2,q3,q4,axis))
