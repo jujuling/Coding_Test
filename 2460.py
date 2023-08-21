34a564a343fa18d99876c30abef8728968be1710
jujuling <53365447+jujuling@users.noreply.github.com>
2023-08-21 15:34:57 +0900

    Baekjoon : 2460
    지능형 기차 2

---
+++ 2460.py
@@ -0,0 +1,11 @@
+import sys
+
+maxp=0
+train=0
+for i in range(10):
+    outp, inp=map(int,sys.stdin.readline().split())
+    train-=outp
+    train+=inp
+    if train>maxp :
+        maxp=train
+print(maxp)
